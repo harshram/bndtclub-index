ad37a5dd586fef05c366edb6b96ac0fa9f9209e7
Claudio Paliotta <claudiopaliotta@Claudios-MacBook-Air-3.local>
2024-08-25 17:38:09 +0200

    pandas dataframe from xlxs

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.write("Hello World!")
+
+data = 'Index.xlsx'
+
+Employment_ICT = pd.read_excel(data, sheet_name='Employment_ICT', skiprows=11, usecols="B:N")
+
+#print(Employment_ICT)
+st.dataframe(Employment_ICT)
