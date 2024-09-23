b7fc9aa0e657bdad346f9073d37899a6533179d3
Paolo Maresca <plo.maresca@gmail.com>
2024-09-23 22:32:21 +0200

    Skeleton to refactor the code

--- data_rendering.py
+++ data_rendering.py
@@ -1,18 +1,45 @@
 
-def plot():
+# Mapping table to address common labels used over and over again
+data_to_plot_labels = {
+    'Employment': {
+        'title': 'Employment Data for IT, FR, DE (Industry J)',
+        'x_label': 'Quarter',
+        'y_label': 'Percentage of Total Employees'
+    },
+    'GVA': {
+        'title': 'GVA Data for IT, FR, DE (Industry J)',
+        'x_label': 'Quarter',
+        'y_label': 'Percentage of GDP'
+    },
+    'LabourDemand': {
+        'title': 'Labour Demand for IT, FR, DE (Industry J)',
+        'x_label': 'Quarter',
+        'y_label': 'Percentage of total job advertisement online'
+    }
+}
+
+# Helper function to plot
+def plot(plt, title='', x_label='', y_label=''):
     pass
 
-def render_version1():
-    pass
+# Render version 1 of the indicator
+def render_version1(st, plt, countries, filtered_data):
+    for k in filtered_data:
+        for c in countries:
+            pass
 
+# Render version 2 of the indicator
 def render_version2():
     pass
 
+# Render version 3 of the indicator
 def render_version3():
     pass
 
+# Render version 4 of the indicator
 def render_version4():
     pass
 
+# Render version 5 of the indicator
 def render_version5():
     pass
