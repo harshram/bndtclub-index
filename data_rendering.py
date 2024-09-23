55f6077a29aab34933b69a439676d930c4501779
Paolo Maresca <plo.maresca@gmail.com>
2024-09-23 22:05:31 +0200

    Added plotting modularisation

---
+++ data_rendering.py
@@ -0,0 +1,18 @@
+
+def plot():
+    pass
+
+def render_version1():
+    pass
+
+def render_version2():
+    pass
+
+def render_version3():
+    pass
+
+def render_version4():
+    pass
+
+def render_version5():
+    pass
